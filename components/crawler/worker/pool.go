d15150539bccc6d3938c4b6af562d34077faa573
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-06-02 17:00:25 +0100

    Tweak more carefully.

--- components/crawler/worker/pool.go
+++ components/crawler/worker/pool.go
@@ -83,7 +83,7 @@ func (w *Pool) getSearchClient() (*elasticsearch.Client, error) {
 		BulkIndexerWorkers:    2,
 		BulkIndexerFlushBytes: 5 * 1024 * 1024, // 5 MB
 
-		BulkGetterBatchSize:    512,
+		BulkGetterBatchSize:    256,
 		BulkGetterBatchTimeout: 1 * time.Second,
 	}
 
